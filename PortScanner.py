ece9351a6d061e9f1561d78f6eb26c57b824af44
mrinos95 <mrinos98@gmail.com>
2015-08-28 21:51:12 -0400

    updated socket

--- PortScanner.py
+++ PortScanner.py
@@ -1,14 +1,15 @@
 import socket
-from datetime import datetime
+
 
 def main():
 
-    server = str(input("Enter a server to check its ports: "))
+    server = input("Enter a server to check its ports: ")
     serverIp = socket.gethostbyname(server) #DNS lookup to convert google.com to ip adress 
     print("Starting the scan!")
     for port in range(1,1025):
-        sock = socket(socket.AF_INET, socket.SOCK_STREAM)
-        if sock.connect_ex(serverIp, port) == 0:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        response = sock.connect_ex(serverIp,port) 
+        if response == 0:
             print("The port %d: is Open"% (port,))
         sock.close()
 main()
