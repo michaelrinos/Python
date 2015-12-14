8113b1e88619a0043bf4ea76c9399b94d40cc331
michaelrinos <mrinos98@gmail.com>
2015-12-14 13:26:43 -0500

    initial commit

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -8,16 +8,23 @@ and for the multiples of five print “Buzz”. For numbers which
 are multiples of both three and five print “FizzBuzz”.
 
 """
+def checkio(number):
+    if number%3==0 and number%5==0 and number != 0:
+        return str("Fizz Buzz")
+    elif number%3==0 and number!=0:
+        return str("Fizz")
+    elif number%5==0 and number !=0:
+        return str("Buzz")
+    else:
+        return str(number)
 
-def main():
-	for i in range(0,int(input("Enter a number to stop at: "))+1):
-		if i%3==0 and i%5==0 and i != 0:
-			print("FizzBuzz")
-		elif i%3==0 and i !=0:
-			print("Fizz")
-		elif i%5==0 and i !=0:
-			print("Buzz")
-		else:
-			print(i)
-	input("Press enter to finish")
-main()
+#Some hints:
+#Convert a number in the string with str(n)
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(15) == "Fizz Buzz", "15 is divisible by 3 and 5"
+    assert checkio(6) == "Fizz", "6 is divisible by 3"
+    assert checkio(5) == "Buzz", "5 is divisible by 5"
+    assert checkio(7) == "7", "7 is not divisible by 3 or 5"
+
