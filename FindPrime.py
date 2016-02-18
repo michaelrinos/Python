0cc4cc13fdb22414f5a75b8fb6b8808b9bf4e988
michaelrinos <mrinos98@gmail.com>
2016-02-18 12:09:22 -0500

    Found a more intuitive solution

--- FindPrime.py
+++ FindPrime.py
@@ -6,28 +6,21 @@ File: Program which given a range finds all prime numbers in that range
 
 """
 from math import sqrt
+import sys
 
 def main():
 	sRange = 2
-	eRange = int(input("Enter an end index: "))
-	if eRange < 2:
-		print("End index must be >= 2")
+	eRange = int(input("Enter a stoping point: "))
+	if eRange <= 2:
+		print("End index must be > 2")
+		sys.exit(0)
 	else:
+
 		primes = []
+
 		for i in range(2,eRange+1):
-			if isPrime(i):
+			#Thanks to checkio for teaching me this neat trick
+			if all(i%j for j in range(2,i)):
 				primes.append(i)
-	print(primes)
-			
-
-def isPrime(number):
-	if number ==2:
-		return True
-	if number% 2 ==0:
-		return False
-	for i in range (3,int(sqrt(number))+1,2):
-		if number % i == 0:
-			return False
-	return True
-
+		print(primes)
 main()
