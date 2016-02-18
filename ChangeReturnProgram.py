95d606f73673b02dd5995dab3312f995f55e304a
michaelrinos <mrinos98@gmail.com>
2016-02-18 13:33:58 -0500

    Finished ChangeReturn...

--- ChangeReturnProgram.py
+++ ChangeReturnProgram.py
@@ -13,23 +13,6 @@ coinChange = [0, 0, 0, 0]
 def main():
     total = float(input("Enter the total due: "))
     amount = float(input("Enter the amount given: "))
-    Calculate(total, amount)
-
-def Calculate(total, amount):
-    x = amount%total
-    for w in bills:
-        if x//w>0:
-            paperChange.insert(bills.index(w),x//w)
-            paperChange.pop(bills.index(w)+1)
-
-    for w in coins:
-        if x//w>0:
-            coinChange.insert(coins.index(w),x//w)
-            coinChange.pop(coins.index(w)+1)
-
-    
-    print(paperChange)
-    
-    
+    print("%.2f"%(amount%total))
 
 main()
