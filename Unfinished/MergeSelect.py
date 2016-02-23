ac2f411294959f91cfad49008be751101972e13b
michaelrinos <mrinos98@gmail.com>
2016-02-23 11:52:14 -0500

    Count Inversions

--- Unfinished/MergeSelect.py
+++ Unfinished/MergeSelect.py
@@ -6,25 +6,10 @@ Description:
 
 """
 
+inversions = 0
+
 def main():
 	pass
-def SelectionSort(list1):
-	#sorts the list
-	length=len(list1)-1
-	x=0
-	for j in range (length,-1,-1):
-		minval=j
-		for i in range(j-1,-1,-1):
-			if list1[i]>list1[minval]:
-				minval=i
-		if minval!=j:
-			swap(list1,minval,j)
-	return list1
-
-def swap(list1, val1, val2):
-	temp = list1[val1]
-	list1[val1]=list1[val2]
-	list1[val2]=temp
 
 def mergeSort(aList):
 	if aList == []:
@@ -37,6 +22,7 @@ def mergeSort(aList):
 		return mergeList(mergeSort(bList), mergeSort(cList))
 
 def mergeList(aList, bList):
+        
 	fList = []
 	aCount = 0
 	bCount = 0
@@ -47,19 +33,17 @@ def mergeList(aList, bList):
 		else:
 			fList.append(bList[bCount])
 			bCount = bCount + 1
+			global inversions
+			inversions += len(aList)
 	if aCount < len(aList):
 		fList.extend(aList[aCount:])
 	elif bCount < len(bList):
 		fList.extend( bList[bCount:])
 	return fList
 
-def MergeSelect(lst, limit):
-	if len(lst) == 0:
-		return list()
-	elif limit > 0:
-		return 
-	else:
-		return mergeSort(lst)
 
-
-print( mergeSort( [1, 5, 3, 4, 2, 2, 7, 5, 3, 4, 9, 0, 1, 2, 5, 4, 76, 6] ) )
+lst1 = [1, 5, 3, 4, 2, 2, 7, 5, 3, 4, 9, 0, 1, 2, 5, 4, 76, 6]
+lst2 = [5, 4, 3, 2, 1]
+print(lst2)
+print( mergeSort( lst2) )
+print(inversions)
